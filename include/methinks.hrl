f93983772d0b1d7e98166b69786bf16c15787429
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-10-26 20:07:49 -0700

    Add smaller target for speed and explanatory comment for REPORT_CHUNK_SIZE

--- include/methinks.hrl
+++ include/methinks.hrl
@@ -1,6 +1,7 @@
 -define(ALPHABET,           "abcdefghijklmnopqrstuvwxyz").
 -define(DEFAULT_GEN_SIZE,   3000).
--define(DEFAULT_TARGET,     "methinksitislikeaweasel").
+%-define(DEFAULT_TARGET,     "methinksitislikeaweasel").
+-define(DEFAULT_TARGET,     "methinks"). % for shorter runs
 -define(LETTER_OFFSET,      $a). % offset for the letter a, used as delta
 -define(MAX_MUTATION,       3).  % existing character +-
--define(REPORT_CHUNK_SIZE,  25).
+-define(REPORT_CHUNK_SIZE,  25). % how often to report
